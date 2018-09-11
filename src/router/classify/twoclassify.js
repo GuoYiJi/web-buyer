a4e14b97c89fc6fafa79f5610d53892c032d1d4d
马世辉 <627970760@qq.com>
2018-09-11 16:43:57 +0800

    删除文件src/router/classify/twoclassify.js

--- src/router/classify/twoclassify.js
+++
@@ -1,8 +0,0 @@
-const twoclassify = [{
-  path: '/pages/classify/twoclassify',
-  name: 'twoclassify',
-  config: {
-    // navigationBarBackgroundColor: '#FA8134',
-  }
-}, ]
-module.exports = twoclassify
