e3fe8f3f70e632717c6cea74bfa75c338fb2211d
姜灿 <1034520546@qq.com>
2018-09-11 15:26:46 +0800

    2018年9月11日15:26:41

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 
 var fileHost = 'https://huiti-img.oss-cn-shanghai.aliyuncs.com'
 export default {
-  // url: 'http://112.74.105.6:8080/jiulei',
-  // url: 'http://10.0.0.125:8080/huiti',
-  url: 'http://1929u21c31.imwork.net/huiti',
-  uploadImageUrl: `${fileHost}`, //默认存在根目录，可根据需求改
+  // url: 'http://1929u21c31.imwork.net/huiti',
+  url: 'http://app.gzbeidouxing.top/huiti',
+  uploadImageUrl: `${fileHost}`, // 默认存在根目录，可根据需求改
   AccessKeySecret: 'GYyUHcqlyvWCGJ8QCsxOU9En2iD9Rw',
   OSSAccessKeyId: 'LTAIclEjqAkc2j94',
   imgPolicy: 'eyJleHBpcmF0aW9uIjoiMjA1MC0wMS0wMVQxMjowMDowMC4wMDBaIiwiY29uZGl0aW9ucyI6W1siY29udGVudC1sZW5ndGgtcmFuZ2UiLDAsNDA5NjAwXV19',
