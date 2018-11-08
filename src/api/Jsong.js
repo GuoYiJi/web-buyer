3dac89932eb540a86fdeebfea137166d42211722
郭忆吉 <guoyiji@guoyijideMacBook-Pro.local>
2018-11-09 01:36:39 +0800

    登录模块修改

--- src/api/Jsong.js
+++ src/api/Jsong.js
@@ -9,74 +9,52 @@ import Vue from 'vue'
 import wx from 'wx'
 import qs from 'qs'
 import loginAPI from './httpJsong';
+import LoginSDK from './LoginSDK';
 const vm = new Vue()
-
+const loginSDK = new LoginSDK();
 
 
 export default {
   async post(url, params = {}) {
-
-    const account = wx.getAccountInfoSync();
-    const { miniProgram: { appId } } = account;
-    params.appId = appId
+    
     return new Promise(async (resolve, reject) => {
       try {
-        loginAPI.authLogin()
-          .then(async res => {
-            
-            var value = await wx.getStorageSync(`${process.env.NODE_ENV}_sessionId`)
-            if (value) {
-              params.sessionId = value
-            }
-            wx.request({
-              url: URL + '/' + url,
-              data: qs.stringify(params),
-              header: {'content-type': 'application/x-www-form-urlencoded'},
-              method: 'POST',
-              // dataType: 'json',
-              success: function(data){
-                // return data
-                // console.log(data.data.desc)
-                const code = Number(data.data.code)
-                switch (code) {
-                  case 0:
-                    const msg = data.data.desc
-                    wx.showToast({
-                      title: `${msg}`,
-                      icon: 'none',
-                      duration: 2000
-                    })
-                    break;
-                  case 1:
-                    resolve(data.data)
-                    break;
-                }
-                // if(code === 1) {
-                //   //  data.data
-                //   // console.log('这是请求')
-                //   resolve(data.data)
-                //   // return data.data
-                // } 
-                // else if (code === 2 ) {
-                //   // 重新登陆 清除登陆信息 location.reload()
-                //   // window.location.reload()
-                //   wx.showToast({
-                //     title: '登录过期，请重新登录',
-                //     icon: 'none',
-                //     duration: 2000
-                //   })
-                //   wx.setStorageSync(`${process.env.NODE_ENV}_sessionId`, '')
-                //   console.log(this);
-                //   setTimeout(()=>{ 
-                //     wx.reLaunch({url: '/pages/home/login'})
-                //   },2000) 
-                // } else if (code === 0) {
-                // }
-                
+
+        loginSDK.isLogin(res => {
+          
+          const appId = wx.getStorageSync('appId');
+          params = {
+            ...params,
+            sessionId: res.user.sessionId,
+            appId
+          }
+          wx.request({
+            url: URL + '/' + url,
+            data: qs.stringify(params),
+            header: {'content-type': 'application/x-www-form-urlencoded'},
+            method: 'POST',
+            // dataType: 'json',
+            success: function(data){
+              // return data
+              // console.log(data.data.desc)
+              const code = Number(data.data.code)
+              switch (code) {
+                case 0:
+                  const msg = data.data.desc
+                  wx.showToast({
+                    title: `${msg}`,
+                    icon: 'none',
+                    duration: 2000
+                  })
+                  break;
+                case 1:
+                  resolve(data.data)
+                  break;
               }
-            })
+              
+            }
           })
-        
+        })
       } catch(err) {
         console.log(err)
       }    
--- src/api/Shui.js
+++ src/api/Shui.js
@@ -8,7 +8,9 @@ import Vue from 'vue'
 import wx from 'wx'
 import qs from 'qs'
 import loginAPI from './httpJsong';
+import LoginSDK from './LoginSDK';
 const vm = new Vue()
+const loginSDK = new LoginSDK();
 export default {
   async post (url, params = {}, headers = {}) {
 
@@ -17,71 +19,68 @@ export default {
     params.appId = appId
     return new Promise(async (resolve, reject) => {
       try {
-        loginAPI.authLogin()
-          .then(async res => {
-            console.log('xxx', params);
-            var value = await wx.getStorageSync(`${process.env.NODE_ENV}_sessionId`)
-            if (value) {
-              params.sessionId = value
-            }
-            wx.request({
-              url: URL + '/' + url,
-              data: headers['content-type'] === 'application/json' ? JSON.stringify(params) : qs.stringify(params),
-              header: {
-                'content-type': 'application/x-www-form-urlencoded',
-                ...headers
-              },
-              method: 'POST',
-              // dataType: 'json',
-              success: (data) => {
-                if (data.statusCode === 500) {
-                  wx.showToast({
-                    title: '请求出错',
-                    icon: 'none',
-                    duration: 1500
-                  })
-                  reject();
-                  return;
-                }
-                // return data
-                // console.log(data.data.desc)
-                const code = Number(data.data.code)
-                if (code === 1) {
-                  //  data.data
-                  // console.log('这是请求')
-                  resolve(data.data)
-                  // return data.data
-                } else if (code === 2) {
-                  console.log(code, 'sdfssdafsd')
-                  // 重新登陆 清除登陆信息 location.reload()
-                  // window.location.reload()
-                  wx.showToast({
-                    title: '登录过期，请重新登录',
-                    icon: 'none',
-                    duration: 2000
-                  })
-                  wx.setStorageSync(`${process.env.NODE_ENV}_sessionId`, '')
-                  // clearUser();
-                  wx.reLaunch({url: '/pages/home/login'});
-                } else if (code === 0) {
-                  const msg = data.data.desc
-                  wx.showToast({
-                    title: `${msg}`,
-                    icon: 'none',
-                    duration: 2000
-                  })
-                }
-
-              },
-
-              fail: res => {
+        loginSDK.isLogin(res => {
+          
+          const appId = wx.getStorageSync('appId');
+          params = {
+            ...params,
+            sessionId: res.user.sessionId,
+            appId
+          }
+          wx.request({
+            url: URL + '/' + url,
+            data: headers['content-type'] === 'application/json' ? JSON.stringify(params) : qs.stringify(params),
+            header: {
+              'content-type': 'application/x-www-form-urlencoded',
+              ...headers
+            },
+            method: 'POST',
+            // dataType: 'json',
+            success: (data) => {
+              if (data.statusCode === 500) {
+                wx.showToast({
+                  title: '请求出错',
+                  icon: 'none',
+                  duration: 1500
+                })
                 reject();
-              },
-              complete: () => {
+                return;
               }
-            })
-          });
+              // return data
+              // console.log(data.data.desc)
+              const code = Number(data.data.code)
+              if (code === 1) {
+                //  data.data
+                // console.log('这是请求')
+                resolve(data.data)
+                // return data.data
+              } else if (code === 2) {
+                // 重新登陆 清除登陆信息 location.reload()
+                // window.location.reload()
+                wx.showToast({
+                  title: '登录过期，请重新登录',
+                  icon: 'none',
+                  duration: 2000
+                })
+                
+              } else if (code === 0) {
+                const msg = data.data.desc
+                wx.showToast({
+                  title: `${msg}`,
+                  icon: 'none',
+                  duration: 2000
+                })
+              }
 
+            },
+
+            fail: res => {
+              reject();
+            },
+            complete: () => {
+            }
+          })
+        })
       } catch (err) {
         reject();
       }
